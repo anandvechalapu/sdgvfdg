2ed6aefeb9457f77702e66f329fe149f80de44eb
anandvechalapu <anand.vechalapu@gmail.com>
2023-03-06 16:01:25 +0530

    initial commit

---
+++ History screen and fetching request information form java API and DB0.7837357300620448.java
@@ -0,0 +1,17 @@
+
+
+Steps to Generate Spring Boot Application with Controller, Service and Repository Classes:
+1. Create a new Maven or Gradle project in the IDE of your choice.
+2. Add the necessary dependencies to the pom.xml or build.gradle file.
+3. Create a controller class with the necessary methods and annotations for handling requests.
+4. Create a service class with the necessary methods and annotations for handling business logic.
+5. Create a repository class with the necessary methods and annotations for accessing the database.
+6. Add the necessary configurations to the application.yml or application.properties file.
+7. Create a HistoryController class with methods and annotations for handling requests on the History screen.
+8. Create a HistoryService class with methods and annotations for handling business logic on the History screen.
+9. Create a HistoryRepository class with methods and annotations for accessing the database to fetch the requested code requests.
+10. Implement the logic for fetching the requested code requests from the Java API and the database.
+11. Implement the logic for displaying the requested code requests with the status and GitHub link on the History screen.
+12. Implement the logic for refreshing the History screen based on the given configuration.
+13. Test the implementation thoroughly to ensure it meets the acceptance criteria.
+14. Package the application and deploy it to a server.
